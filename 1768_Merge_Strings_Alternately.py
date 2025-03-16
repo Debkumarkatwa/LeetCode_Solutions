7ca6dc099776acdc4e4c1813efac43f3bd9559a1
Deb kumar Mondal <visitdebkumarkatwa@gmail.com>
2025-03-16 18:46:05 +0530

    try some different approch

--- 1768_Merge_Strings_Alternately.py
+++ 1768_Merge_Strings_Alternately.py
@@ -1,3 +1,4 @@
+# '''
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         a = []
@@ -7,8 +8,19 @@ class Solution:
             if i < len(word2):
                 a.append(word2[i])
 
-        return ''.join(a)
-    
+        return ''.join(a)   
+# '''
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:       
+        a, result = min(len(word1), len(word2)), ''
+        for i in range(a):
+            result += word1[i]
+            result += word2[i]
+
+        result += word1[a:]
+        result += word2[a:]
+        return result
+# ''' 
 a = Solution()
 print(a.mergeAlternately("abc","pqr"))
 print(a.mergeAlternately("ab","pqrs"))
--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,6 +1,21 @@
+# ''' 
 class Solution:
     def twoSum(self, nums, target: int):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if (nums[i]+nums[j])==target:
                     return [i,j]
+# ''' 
+class Solution:
+    def twoSum(self, nums: list, target: int):
+        for i in range(len(nums)):
+            a = list(map(lambda x: x + nums[i], nums))
+            a.pop(i)
+            
+            if target in a:
+                return[i,nums.index(target-nums[i])]
+# ''' 
+
+a = Solution()
+print(a.twoSum([2,7,11,15],9)) # [0,1]
+print(a.twoSum([3,2,4],6)) # [1,2]
